b537695c3a805b9f0c0d7da6f87f8ddcaee84a2f
blake <bpw537@mail.usask.ca>
2021-01-26 16:09:15 -0600

    Version 3, has custom delimiters
    Add() method can now take custom delimiters in the string. Fixed up the print results for version 2 tests and make version 3 tests. All tests work.

--- StringCalculator.py
+++ StringCalculator.py
@@ -14,8 +14,14 @@ def Add(numbers):
     if numbers == "":
         return 0
 
+    delimiter = ","
+    if len(numbers) > 4:
+        if numbers[0:2] == "//":
+            delimiter = numbers[2:numbers.index("\n")]
+            numbers = numbers[numbers.index("\n"):]
+
     # Get each number and add them together
-    list = numbers.split(',')
+    list = numbers.split(delimiter)
     sum = 0
     for i in list:
         # Removes any newlines then add the numbers
@@ -43,16 +49,30 @@ def Tests():
 
     # Testing version 2 (handles new lines)
     if Add("\n1") != 1:
-        print("Add() failed on 1 number input with new line, got", Add("1"))
+        print("Add() failed on 1 number input with new line, got", Add("\n1"))
 
     if Add("\n-1\n") != -1:
-        print("Add() failed on -1 number input with new lines, got", Add("-1"))
+        print("Add() failed on -1 number input with new lines, got", Add("\n-1\n"))
 
     if Add("5,2\n,\n9") != 16:
-        print("Add() failed on '5,2,9' number input, got with new lines", Add("5,2,9"))
+        print("Add() failed on '5,2,9' number input, got with new lines", Add("5,2\n,\n9"))
 
     if Add("5\n,-2,\n9") != 12:
-        print("Add() failed on '5,-2,9' number input with new lines, got", Add("5,-2,9"))
+        print("Add() failed on '5,-2,9' number input with new lines, got", Add("5\n,-2,\n9"))
+
+    # Testing version 3 (custom delimiter)
+    if Add("//;\n1") != 1:
+        print("Add() failed on 1 number input with custom delimiter, got", Add("//;\n1"))
+
+    if Add("//$@\n1") != 1:
+        print("Add() failed on 1 number input with custom delimiter longer than 1 character, got", Add("//$@\n1"))
+
+    if Add("//;\n5;2;9") != 16:
+        print("Add() failed on '5,2,9' number input with custom delimiter, got", Add("//;\n5;2;9"))
+
+    if Add("//$@\n5$@2$@9") != 16:
+        print("Add() failed on '5,2,9' number input with custom delimiter longer than 1 character, got", Add("//$@\n5$@2$@9"))
+
     print("Add() tests complete")
 
 Tests()
