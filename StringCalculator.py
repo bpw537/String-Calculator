287de9a9a98221129df5036016e3e6e6af278384
blake <bpw537@mail.usask.ca>
2021-01-25 22:49:34 -0600

    Made Add() method
    Made Add() method with a description. This is a version that doesnt run yet.

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,2 +1,13 @@
 # Blake Williams
 # bpw537@usask.ca
+
+"""
+Purpose: Take in a string of numbers and commas and add the numbers together
+
+Pre-conditions:
+    :param numbers: A string made of only numbers separated by commas
+
+Return: A integer that is the initial numbers added together
+"""
+def Add(numbers):
+    return 0
